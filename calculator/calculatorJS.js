ce8f7ff6268941cef718ce6657a8551cb4e8e199
Prashanth Sanivarapu <43651539+psanivarapu@users.noreply.github.com>
2024-04-06 14:21:01 +0530

    Add files via upload

--- calculator/calculatorJS.js
+++ calculator/calculatorJS.js
@@ -1,35 +1,50 @@
-/* function appendToDisplay(value) {
-    document.getElementById("display").value += value;
-}
+const display = document.querySelector('#display');
 
-function clearDisplay() {
-    document.getElementById("display").value = "";
+let operand1 = 0;
+let operand2 = 0;
+let operator = "";
+
+const allButtons = document.querySelectorAll('input[type="button"]');
+for (let btn of allButtons) {
+    btn.addEventListener('click', function () {
+        const btnValue = btn.value;
+        if (!isNaN(parseInt(btnValue))) {
+            display.value += btnValue;
+        }
+        else if (btnValue === "=") {
+            calculate();
+        } else {
+            operand1 = parseInt(display.value);
+            operator = btnValue;
+            display.value = "";
+        }
+    });
 }
 
 function calculate() {
-    var expression = document.getElementById("display").value;
-    var result = eval(expression);
-    document.getElementById("display").value = result;
-} */
-
-const display = document.querySelector('#display');
-/* const btn7 = document.querySelector('#seven');
-const btn8 = document.querySelector('#eight');
-const btn9 = document.querySelector('#nine');
-
-btn7.addEventListener('click', () => {
-    display.value += btn7.value;
-});
-btn8.addEventListener('click', () => {
-    display.value += btn8.value;
-});
-btn9.addEventListener('click', () => {
-    display.value += btn9.value;
-}); */
-
-const numbers = document.querySelectorAll('#nbr');
-for (let number of numbers) {
-    number.addEventListener('click', () => {
-        display.value += number.value;
-    })
+    switch (operator) {
+        case '+':
+            operand2 = parseInt(display.value);
+            display.value = operand1 + operand2;
+            break;
+        case '-':
+            operand2 = parseInt(display.value);
+            display.value = operand1 - operand2;
+            break;
+        case '*':
+            operand2 = parseInt(display.value);
+            display.value = operand1 * operand2;
+            break;
+        case '/':
+            operand2 = parseInt(display.value);
+            if (operand2 === 0) {
+                display.value = "cannot divide by zero"
+            } else {
+                display.value = operand1 / operand2;
+            }
+            break;
+        case 'C':
+            display.value = "";
+            break;
+    }
 }
